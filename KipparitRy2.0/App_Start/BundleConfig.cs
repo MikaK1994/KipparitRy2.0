6e7b1ac48a76f96b2993abe7e92b7f4e0176f1d0
mika tiihonen <koneautomaatio@hotmail.com>
2021-12-18 10:22:00 +0200

    css muutoksia

--- KipparitRy2.0/App_Start/BundleConfig.cs
+++ KipparitRy2.0/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace KipparitRy2._0
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
